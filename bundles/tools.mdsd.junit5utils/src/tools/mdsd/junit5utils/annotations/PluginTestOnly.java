629823ee7b39e1a01670dd5323700905282fe31a
Sebastian Krach <krach@fzi.de>
2020-10-27 10:03:33 +0100

    Added support to flag entire class as plugin test only

--- bundles/tools.mdsd.junit5utils/src/tools/mdsd/junit5utils/annotations/PluginTestOnly.java
+++ bundles/tools.mdsd.junit5utils/src/tools/mdsd/junit5utils/annotations/PluginTestOnly.java
@@ -1,13 +1,14 @@
 package tools.mdsd.junit5utils.annotations;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
-@Target(METHOD)
+@Target({METHOD, TYPE})
 public @interface PluginTestOnly {
 
 }
